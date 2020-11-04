4d3bb050a39aa90d9fbaeea8a7bb3258acfb1cfc
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-11-04 14:05:37 +0330

    Create ping command.

--- cogs/manager.py
+++ cogs/manager.py
@@ -5,7 +5,12 @@ class Manager(commands.Cog):
     
     def __init__(self,client):
         self.client = client
-
+    
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'{round(self.client.latency*1000)}ms')
+        print('[PING COMMAND]')
+        
 def setup(client):
     client.add_cog(Manager(client))
 
