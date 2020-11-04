a80d59acdd8aaecd910e9007e560bcd1cc401eb4
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-11-04 13:00:37 +0330

    Create get prefix function for read prefixes.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import discord
+import json
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+#Read prefixes from prefixes json file
+def get_prefix(client,message):
+    with open('prefixes.json','r') as f:
+        prefixes = json.load(f)
+    return prefixes[str(message.guild.id)]
 
+#Create bot
+client = commands.Bot(command_prefix = get_prefix)
+
+#Event that show bot is start
 @client.event
 async def on_ready():
     print('[BOT IS RUNNING]')
