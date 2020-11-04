fa4fa5de2af29b3a6c453dfa318abeba20e471fd
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-11-04 13:06:09 +0330

    Create event that add defualt prefix when bot join server.

--- main.py
+++ main.py
@@ -16,4 +16,14 @@ client = commands.Bot(command_prefix = get_prefix)
 async def on_ready():
     print('[BOT IS RUNNING]')
 
+#Event that create defualt value for prefix when bot join server
+@client.event
+async def on_guild_join(guild):
+    with open('prefixes.json','r') as f:
+        prefixes = json.load(f)
+    prefixes[str(guild.id)] = '.'
+
+    with open('prefixes.json','w') as f:
+        json.dump(prefixes,f,indent=4)
+
 client.run('NzczNDc2NTA5Njk1NDc1NzIz.X6JyIg.8pZfUSSS04XlU3DKF5GMTaiun7E')
