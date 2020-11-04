63b6d0678f5e28953aa1e0c28d4c25269c26b49c
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-11-04 16:57:41 +0330

    Create new command for kick members from server.

--- cogs/manager.py
+++ cogs/manager.py
@@ -10,6 +10,12 @@ class Manager(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f'{round(self.client.latency*1000)}ms')
         print('[PING COMMAND]')
+    
+    #Command for kick members from server
+    @commands.command()
+    async def kick(ctx, member:discord.Member, * ,reason = None):
+        await member.kick(reason = reason)
+        print('[MEMBER KICKED]')
         
 def setup(client):
     client.add_cog(Manager(client))
