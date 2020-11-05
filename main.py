52df872f5a5f5f5924227abd209f26996695c87f
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-11-05 17:40:28 +0330

    Create log file and add logging to events.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import os
+import datetime
 from discord.ext import commands
 
 #Read prefixes from prefixes json file
@@ -15,16 +16,24 @@ client = commands.Bot(command_prefix = get_prefix)
 #Event that show bot is starting
 @client.event
 async def on_ready():
+    with open('logs.txt','a+') as f:
+        now = datetime.datetime.now()
+        f.write(f'\n[%s]->[BOT IS RUNNING]\n'%(now))
     print('[BOT IS RUNNING]')
 
-#Event for log new user
-@client.event
+#Event fo log join user        
 async def on_member_join(member):
+    with open('logs.txt','a+') as f:
+        now = datetime.datetime.now()
+        f.write(f'\n[%s]->[[member] HAS JOINED SERVER]\n'%(now))
     print(f'[[member] HAS JOINED SERVER]')
 
 #Event for log remove user
 @client.event
 async def on_member_remove(member):
+    now = datetime.datetime.now()
+    with open('logs.txt','a+') as f:
+        f.write(f'\n[%s]->[[member] HAS LEFTED SERVER]\n'%(now))
     print(f'[[member] HAS LEFTED SERVER]')
 
 #Event that create defualt value for prefix when bot join server
@@ -36,6 +45,10 @@ async def on_guild_join(guild):
 
     with open('prefixes.json','w') as f:
         json.dump(prefixes,f,indent=4)
+
+    now = datetime.datetime.now() 
+    with open('logs.txt','a+') as f:
+        f.write(f'\n[%s]->[ADD DEFAULT PREFIX]\n'%(now))
     
     print(f'[ADD DEFAULT PREFIX]')
 
@@ -49,6 +62,10 @@ async def on_guild_remove(guild):
     with open('prefixes.json','w') as f:
         json.dump(prefixes,f,indent=4)
 
+    now = datetime.datetime.now() 
+    with open('logs.txt','a+') as f:
+        f.write(f'\n[%s]->[DELETE PREFIX]\n'%(now))
+    
     print(f'[DELETE PREFIX]')
 
 #Command for change prefix
@@ -60,6 +77,10 @@ async def changeprefix(ctx, prefix):
 
     with open('prefixes.json','w') as f:
         json.dump(prefixes,f,indent=4)
+
+    now = datetime.datetime.now() 
+    with open('logs.txt','a+') as f:
+        f.write(f'\n[%s]->[PREFIX CHANGED TO %s]\n'%(now,prefix))
 
     print(f'[PREFIX CHANGED TO {prefix}]')
 
