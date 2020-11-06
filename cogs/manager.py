b8bbbfb21eb40139a9cc7c5427ac31f9fbfa64b0
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-11-06 11:23:05 +0330

    Create info command for showing server information and add self arg to functions.

--- cogs/manager.py
+++ cogs/manager.py
@@ -13,19 +13,19 @@ class Manager(commands.Cog):
     
     #Command for kick members from server
     @commands.command()
-    async def kick(ctx, member:discord.Member, * ,reason = None):
+    async def kick(self,ctx, member:discord.Member, * ,reason = None):
         await member.kick(reason = reason)
         print('[MEMBER KICKED]')
     
     #Command for ban members from server
     @commands.command()
-    async def ban(ctx, member:discord.Member, * ,reason = None):
+    async def ban(self,ctx, member:discord.Member, * ,reason = None):
         await member.ban(reason = reason)
         print('[MEMBER BANNED]')
     
     #Command for unban users
     @commands.command()
-    async def unban(ctx, *,member):
+    async def unban(self,ctx, *,member):
         banned_users = await ctx.guild.bans()
         member_name , member_discriminator = member.split('#')
         for ban_entry in banned_users:
@@ -35,7 +35,29 @@ class Manager(commands.Cog):
                 await ctx.send(f'Unbanned {user.mention}')
                 print(f'[USER {user.mention} UNBANNED]')
                 return
-   
+    #Command for show server information 
+    @commands.command()
+    async def info(self,ctx):
+        name = str(ctx.guild.name)
+        description = str(ctx.guild.description)
+        owner = str(ctx.guild.owner)
+        Id = str(ctx.guild.id)
+        region = str(ctx.guild.region)
+        icon = str(ctx.guild.icon_url)
+        member_count = str(ctx.guild.member_count)
+
+        embed = discord.Embed(
+            title = name + ' Server information',
+            description = description,
+            color = discord.Color.red()
+            )
+        embed.set_thumbnail(url=icon)
+        embed.add_field(name='Owner',value=owner,inline=True)      
+        embed.add_field(name='Id',value=Id,inline=True)
+        embed.add_field(name='Region',value=region,inline=True)
+
+        embed.add_field(name='Members',value=member_count,inline=True)
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Manager(client))
